e6fdaafc28f67afac290776bfa92c0a229a65b0d
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 18:41:55 +0000

    Extracts logic to create headers and payload to private methods

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -2,19 +2,29 @@ require 'json'
 require 'rest-client'
 
 class Recipient
+  RECIPIENTS_ENDPOINT = 'https://coolpay.herokuapp.com/api/recipients'
 
   def add(name, auth_key)
+    payload = create_payload(name)
+    headers = create_headers(auth_key)
+    response = RestClient.post RECIPIENTS_ENDPOINT, payload, headers
+  end
+
+
+  private
+
+  def create_payload(name)
     values = {
       'recipient': {
         'name': name
       }.to_json
     }
+  end
 
-    headers = {
+  def create_headers(auth_key)
+    {
       :content_type => 'application/json',
       :authorization => "Bearer #{auth_key}"
     }
-
-    response = RestClient.post 'https://coolpay.herokuapp.com/api/recipients', values, headers
   end
 end
