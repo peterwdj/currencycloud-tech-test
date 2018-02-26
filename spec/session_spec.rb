694e43b059fa242afe14944854c98e68564893b0
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 12:46:28 +0000

    Stubs RestClient requests with webmock gem; adds test for invalid credentials

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -15,10 +15,40 @@ describe Session do
 
   describe '#login' do
     it 'returns an authentication key' do
-      RestClient = double('RestClient', :post => { :token => '12345.yourtoken.67890' }.to_json)
+      stub_request(:post, 'https://coolpay.herokuapp.com/api/login').
+        with(
+          body: {
+            'username': ENV['USERNAME'],
+            'apikey': ENV['APIKEY']
+          }.to_json,
+          headers: {
+            :content_type => 'application/json'
+          }
+        ).to_return(
+          status: 200,
+          body: { :token => '12345.yourtoken.67890' }.to_json
+        )
       session = Session.create
       session.login
       expect(session.auth_key).to eq '12345.yourtoken.67890'
+    end
+
+    it 'returns error response text when invalid credentials are supplied' do
+      stub_request(:post, 'https://coolpay.herokuapp.com/api/login').
+        with(
+          body: {
+            'username': 'bad',
+            'apikey': 'credentials'
+          }.to_json,
+          headers: {
+            :content_type => 'application/json'
+          }
+        ).to_return(
+          status: 404,
+          body: 'Internal server error'
+        )
+      session = Session.create
+      expect(session.login('bad', 'credentials')).to eq 'Internal server error'
     end
   end
 end
