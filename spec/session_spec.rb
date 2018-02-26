d1fc009f3bdb2bad8b37aa1244d1f89ab6b85a73
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 09:47:14 +0000

    Creates test for #login with stubbed RestClient

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -7,9 +7,18 @@ describe Session do
     end
   end
 
-  describe '#acces' do
+  describe '#access' do
     it 'returns an instance of Session' do
       expect(Session.access).to be_an_instance_of(Session)
+    end
+  end
+
+  describe '#login' do
+    it 'returns an authentication key' do
+      RestClient = double('RestClient', :post => { :token => '12345.yourtoken.67890' }.to_json)
+      session = Session.create
+      session.login(ENV['USERNAME'], ENV['APIKEY'])
+      expect(session.auth_key).to eq '12345.yourtoken.67890'
     end
   end
 end
