1553daef48ae720e2fc96cab7e5529ef2222e009
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 09:47:56 +0000

    Adds #login method assigning authentication key to @auth_key

--- lib/session.rb
+++ lib/session.rb
@@ -1,9 +1,20 @@
+require 'dotenv/load'
+require 'rest-client'
+require 'json'
+
 class Session
+  attr_reader :auth_key
+
   def self.create
     @session = Session.new
   end
 
   def self.access
     @session
+  end
+
+  def login(username, apikey)
+    response = RestClient.post 'https://coolpay.herokuapp.com/api/login', { 'username': username, 'apikey': apikey }.to_json, { :content_type => 'application/json' }
+    @auth_key = JSON.parse(response)['token']
   end
 end
