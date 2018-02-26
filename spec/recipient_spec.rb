58b28d49401cfb037856516dc4b501538b745ebf
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 19:03:43 +0000

    Adjust test to match bug fix

--- spec/recipient_spec.rb
+++ spec/recipient_spec.rb
@@ -7,7 +7,7 @@ describe Recipient do
       recipient = described_class.new
       recipient.add('Marvin', '42')
       expect(RestClient).to have_received(:post).with(
-        'https://coolpay.herokuapp.com/api/recipients', {:recipient=>"{\"name\":\"Marvin\"}"}, {:content_type=>"application/json", :authorization=>"Bearer 42"}
+        'https://coolpay.herokuapp.com/api/recipients', {:recipient=>{:name => "Marvin"}}, {:content_type=>"application/json", :authorization=>"Bearer 42"}
         )
     end
   end
