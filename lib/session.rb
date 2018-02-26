ca90ec2ff4bfbb6efd0dbe9279f286a6bf87152a
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 13:54:28 +0000

    Refactors login logic to private methods

--- lib/session.rb
+++ lib/session.rb
@@ -17,21 +17,30 @@ class Session
 
   def login(username=ENV['USERNAME'], apikey=ENV['APIKEY'])
     payload = create_payload(username, apikey)
-    begin
-      response = RestClient.post LOGIN_URL, payload, HEADER
-    rescue RestClient::ExceptionWithResponse => err
-      puts err.response.code
-    end
-    if err == nil
-      @auth_key = JSON.parse(response)['token']
-    else
-      return err.response.to_s
-    end
+    err, response = send_request(payload)
+    send_response(err, response)
   end
+
 
   private
 
   def create_payload(username, apikey)
     { 'username': username, 'apikey': apikey }.to_json
+  end
+
+  def send_response(err, response)
+    if err == nil
+      @auth_key = JSON.parse(response)['token']
+    else
+      err.response.to_s
+    end
+  end
+
+  def send_request(payload)
+    begin
+      response = RestClient.post LOGIN_URL, payload, HEADER
+    rescue RestClient::ExceptionWithResponse => err
+    end
+    return err, response
   end
 end
