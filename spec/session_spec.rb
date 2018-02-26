f3bf17240a5e59008fbb622559847a80b5a1c1eb
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 13:46:07 +0000

    Use helper stub methods instead of full stubs

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -15,38 +15,14 @@ describe Session do
 
   describe '#login' do
     it 'returns an authentication key' do
-      stub_request(:post, 'https://coolpay.herokuapp.com/api/login').
-        with(
-          body: {
-            'username': ENV['USERNAME'],
-            'apikey': ENV['APIKEY']
-          }.to_json,
-          headers: {
-            :content_type => 'application/json'
-          }
-        ).to_return(
-          status: 200,
-          body: { :token => '12345.yourtoken.67890' }.to_json
-        )
+      stub_good_credentials
       session = Session.create
       session.login
       expect(session.auth_key).to eq '12345.yourtoken.67890'
     end
 
     it 'returns error response text when invalid credentials are supplied' do
-      stub_request(:post, 'https://coolpay.herokuapp.com/api/login').
-        with(
-          body: {
-            'username': 'bad',
-            'apikey': 'credentials'
-          }.to_json,
-          headers: {
-            :content_type => 'application/json'
-          }
-        ).to_return(
-          status: 404,
-          body: 'Internal server error'
-        )
+      stub_bad_credentials
       session = Session.create
       expect(session.login('bad', 'credentials')).to eq 'Internal server error'
     end
