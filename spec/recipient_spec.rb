c046ea96792c7142dc571f67650cc68066f68acf
Peter Johnstone <peter.wdj@gmail.com>
2018-02-26 18:49:10 +0000

    Specifies arguments with which RestClient.post should have been called

--- spec/recipient_spec.rb
+++ spec/recipient_spec.rb
@@ -6,7 +6,9 @@ describe Recipient do
       allow(RestClient).to receive(:post)
       recipient = described_class.new
       recipient.add('Marvin', '42')
-      expect(RestClient).to have_received(:post)
+      expect(RestClient).to have_received(:post).with(
+        'https://coolpay.herokuapp.com/api/recipients', {:recipient=>"{\"name\":\"Marvin\"}"}, {:content_type=>"application/json", :authorization=>"Bearer 42"}
+        )
     end
   end
 end
