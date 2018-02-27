02a27995fc05517cc8df3f64b40ec5cc6ddbb49b
Peter Johnstone <peter.wdj@gmail.com>
2018-02-27 00:03:07 +0000

    Rewrites tests to remove now deleted class methods

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -1,32 +1,18 @@
 require 'session'
 
 describe Session do
-  before :each do
-    Session.create
-  end
-
-  describe '#create' do
-    it 'create a new instance of Session' do
-      expect(Session.create).to be_an_instance_of Session
-    end
-  end
-
-  describe '#access' do
-    it 'returns an instance of Session' do
-      expect(Session.access).to be_an_instance_of Session
-    end
-  end
+  let(:session) { described_class.new }
 
   describe '#login' do
     it 'returns an authentication key' do
       stub_good_credentials
-      Session.access.login
-      expect(Session.access.auth_key).to eq '12345.yourtoken.67890'
+      session.login
+      expect(session.auth_key).to eq '12345.yourtoken.67890'
     end
 
     it 'returns error response text when invalid credentials are supplied' do
       stub_bad_credentials
-      expect(Session.access.login('bad', 'credentials')).to eq 'Internal server error'
+      expect(session.login('bad', 'credentials')).to eq 'Internal server error'
     end
   end
 end
