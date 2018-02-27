cada885cbe75476abaa3e0364a5b6cb3fbc3c2c3
Peter Johnstone <peter.wdj@gmail.com>
2018-02-27 13:14:56 +0000

    Adds test for invalid auth token

--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -21,5 +21,27 @@ describe Payment do
         }
       )
     end
+
+    it 'returns error response text when an invalid auth key is provided' do
+      allow(payment).to receive(:get_id_by_name)
+      stub_request(:post, 'https://coolpay.herokuapp.com/api/payments')
+        .with(
+          body: {
+            payment: {
+              amount: "1000000",
+              currency: "GBP",
+              recipient_id: nil
+            }
+          },
+          headers: {
+       	    authorization:'Bearer 5n34ky-br1b3',
+       	    content_type: 'application/x-www-form-urlencoded'
+          })
+          .to_return(
+            status: 401,
+            body: '401 Unauthorized'
+          )
+        expect(payment.send_to('Government Officials', 1_000_000, '5n34ky-br1b3')).to eq '401 Unauthorized'
+    end
   end
 end
