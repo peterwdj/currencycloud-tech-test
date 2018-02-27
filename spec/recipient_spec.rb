dd6a0f2703b830468c623408fcad0f83f01fb4ea
Peter Johnstone <peter.wdj@gmail.com>
2018-02-27 09:37:31 +0000

    Adds test for errors due to invalid auth key

--- spec/recipient_spec.rb
+++ spec/recipient_spec.rb
@@ -1,14 +1,34 @@
 require 'recipient'
 
 describe Recipient do
+  let(:recipient) { described_class.new }
+
   describe '#add' do
     it 'makes a POST request to the recipients API' do
       allow(RestClient).to receive(:post)
-      recipient = described_class.new
       recipient.add('Marvin', '42')
       expect(RestClient).to have_received(:post).with(
         'https://coolpay.herokuapp.com/api/recipients', {:recipient=>{:name => "Marvin"}}, {:content_type=>"application/json", :authorization=>"Bearer 42"}
         )
+    end
+
+    it 'returns error response text when an invalid auth key is provided' do
+      stub_request(:post, "https://coolpay.herokuapp.com/api/recipients").
+        with(
+          body: {
+            'recipient': {
+              'name': 'Marvin'
+              }
+            },
+          headers: {
+       	    :authorization => 'Bearer 42',
+       	    :content_type => 'application/x-www-form-urlencoded',
+          }
+        ).to_return(
+          status: 401,
+          body: "401 Unauthorized"
+        )
+      expect(recipient.add('Marvin', '42')).to eq '401 Unauthorized'
     end
   end
 end
