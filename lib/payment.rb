88d756d1e50638ee4ccbc025fd97503a9deab5a8
Peter Johnstone <peter.wdj@gmail.com>
2018-02-27 12:02:15 +0000

    Extracts logic to private methods

--- lib/payment.rb
+++ lib/payment.rb
@@ -6,19 +6,10 @@ class Payment
   RECIPIENTS_ENDPOINT = 'https://coolpay.herokuapp.com/api/recipients'
 
   def send_new(name, amount, auth_key)
-    headers = {
-      :content_type => 'application/json',
-      :authorization => "Bearer #{auth_key}"
-    }
-
-    payload = {
-      'payment': {
-        'amount': amount,
-        'currency': 'GBP',
-        'recipient_id': get_id_by_name(name, headers)
-      }
-    }
-    response = RestClient.post PAYMENTS_ENDPOINT, payload, headers
+    headers = create_headers(auth_key)
+    id = get_id_by_name(name, headers)
+    payload = create_payload(amount, id)
+    response, error = send_request(payload, headers)
   end
 
 
@@ -26,6 +17,31 @@ class Payment
 
   def get_id_by_name(name, headers)
     response = RestClient.get RECIPIENTS_ENDPOINT + "?name=#{name}", headers
-    puts response
+    return JSON.parse(response)['recipients'][0]['id']
+  end
+
+  def create_headers(auth_key)
+    {
+      :content_type => 'application/json',
+      :authorization => "Bearer #{auth_key}"
+    }
+  end
+
+  def create_payload(amount, id)
+    {
+      'payment': {
+        'amount': amount,
+        'currency': 'GBP',
+        'recipient_id': id
+      }
+    }
+  end
+
+  def send_request(payload, headers)
+    begin
+      response = RestClient.post PAYMENTS_ENDPOINT, payload, headers
+    rescue RestClient::ExceptionWithResponse => error
+    end
+    return response, error
   end
 end
